b20aca8c1fe7dc9b2434e2b153d6ee970cea510e
Fabian Ferno <fabianferno@gmail.com>
2021-01-09 23:28:52 +0530

    Update Privacy policy

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -3,8 +3,32 @@ import React from "react";
 //Functional Component 
 export default function PrivacyPolicy() {
   return (
-    <div>
-      <small>Privacy Policy Page</small>
-    </div>
+    <div className="container pt-2 justify-content-center" style={{fontFamily: '"Montserrat"'}}>
+        <h3><strong>What’s a CFP?</strong></h3>
+        <p style={{fontSize: 'medium'}}>
+          A CFP is a conference announcement, also known as a Call for Proposals
+          (CFP).<br />
+          It provides:
+        </p><ul>
+          <li>The conference theme </li>
+          <li>Guidelines for presentations</li>
+          <li>Requirements for abstracts/proposals</li>
+          <li>Deadlines</li>
+        </ul>
+        <p />
+        <h3><strong>Writing the Proposal/Abstract</strong></h3>
+        <p>Define the problem you are addressing clearly and concisely (BONUS: Include keywords or phrases from the CFP).<br />Captivate readers with your research description in terms of the problem by: 
+        </p><ul>
+          <li> Providing interesting or controversial perspectives on your topic. 
+          </li>
+          <li>Explaining what your presentation will cover regarding to the topic and problem presented.</li>
+          <li>Matching your research to the conference theme/CFP’s topics, and making sure the scope of your presentation matches the time allowed for presentations.
+          </li>
+          <li>Participants will be given a google form with the respective questions asked so kindly answer relevantly.*</li>
+        </ul>
+        <p />
+        <i>Happy Writing and Happy Presenting!
+        </i>
+      </div>
   );
 }
